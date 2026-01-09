2bc1b5ff48cc37f7b22fb959d8647de772ceb1ea
Lokeswaran <lokeswaranrofficial@gmail.com>
2026-01-09 12:32:53 +0530

    deleted

--- start-prod.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Timesheet Application (Production Mode)...
-echo Server will run on port 3000.
-echo Access via http://localhost:3000 or http://[YOUR_IP]:3000
-npm run start:local
-pause
